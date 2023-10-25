60c1d399f2038cddf57ea0b381a3b39f82bbf04f
Gerald <gerald.mwang@gmail.com>
2023-10-24 18:48:10 -0600

    Leveraging \r and setTimeout for Spinner. Using this \r, we can fake an animation by replacing/rewriting characters in the same spot after a delay.

---
+++ spinner1.js
@@ -0,0 +1,32 @@
+process.stdout.write('hello from sprinner1.js... \rheyyy\n');
+
+// personal test
+
+setTimeout(() => {
+  process.stdout.write('\rG   ');
+}, 100);
+
+setTimeout(() => {
+  process.stdout.write('\rE   ');
+}, 300);
+
+setTimeout(() => {
+  process.stdout.write('\rR   ');
+}, 500);
+
+setTimeout(() => {
+  process.stdout.write('\rA   ');
+}, 700);
+
+setTimeout(() => {
+  process.stdout.write('\rL   ');
+}, 900);
+
+setTimeout(() => {
+  process.stdout.write('\rD   ');
+}, 1100);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r\\   '); 
+}, 1300);
